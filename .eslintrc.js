9a7c7ea578176e78f94dedd91af07ca94c7f0cd5
Vl@dislav <vt@burno.tech>
2022-06-13 21:38:06 +0300

    Integrated graphql

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,33 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/typescript/recommended", "plugin:storybook/recommended"],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    semi: ['warn', 'never'],
-    indent: ['warn', 4],
-    'ts-ignore': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-namespace': 'off'
-  },
-  overrides: [{
-    files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+    root: true,
     env: {
-      jest: true
-    }
-  }]
-};
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential",
+        "eslint:recommended",
+        "@vue/typescript/recommended",
+        "plugin:storybook/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        semi: ['warn', 'never'],
+        indent: ['warn', 4],
+        'ts-ignore': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-namespace': 'off'
+    },
+    overrides: [{
+        files: [
+            '**/__tests__/*.{j,t}s?(x)',
+            '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        ],
+        env: {
+            jest: true
+        }
+    }]
+}
